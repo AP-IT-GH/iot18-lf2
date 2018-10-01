4957fdcdde8b8269068d81c29a6d556fff44eaf6
Seppe De Beule <seppe.debeule@telenet.be>
2018-10-01 11:29:23 +0200

    Added login page
    Made basic css rules and added a login template for the dashboard

--- src/dashboard/dashboard/src/app/app.module.ts
+++ src/dashboard/dashboard/src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: 'login', component: LoginComponent},
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+    ], { useHash: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
