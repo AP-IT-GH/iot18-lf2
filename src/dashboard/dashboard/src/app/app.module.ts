33bc20af0fd66dda1092a55b511c67a43d922404
Seppe De Beule <seppe.debeule@telenet.be>
2018-10-04 17:27:22 +0200

    Notification template + layout updates
    Poging om iOS-styled switched te installeren. Nog niet werkende.
    Nieuwe font: Roboto Light + nog enkele css updates

--- src/dashboard/dashboard/src/app/app.module.ts
+++ src/dashboard/dashboard/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { NotificationComponent } from './notification/notification.component';
+import { SettingsComponent } from './settings/settings.component';
 
 @NgModule({
   declarations: [
@@ -14,13 +16,16 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     LoginComponent,
     HomeComponent,
     NavbarComponent,
-    SidebarComponent
+    SidebarComponent,
+    NotificationComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: 'home', component: HomeComponent},
       { path: 'login', component: LoginComponent},
+      { path: 'settings', component: SettingsComponent},
       { path: '', redirectTo: 'home', pathMatch: 'full'},
     ], { useHash: true }),
   ],
