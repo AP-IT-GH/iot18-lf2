e6fa3426faff7ecf17954d0ef7da6065d9afe5bd
RaymondMohammad <s088606@ap.be>
2018-11-08 16:25:30 +0100

    Prepared dataset for connection with frontend

--- src/api/LabFarm/LabFarm/Data/DbInitializer.cs
+++ src/api/LabFarm/LabFarm/Data/DbInitializer.cs
@@ -30,7 +30,8 @@ namespace LabFarm.Data
             context.SaveChanges();
 
             context.Sensors.AddRange(
-                new Sensor { SensorType = "Humidity", Value = 5, LabfarmId = 1}
+                new Sensor { SensorType = "Humidity", Value = 5, LabfarmId = 1},
+                new Sensor { SensorType = "Temperature", Value = 7, LabfarmId = 1 }
             );
 
             context.SaveChanges();
@@ -40,7 +41,12 @@ namespace LabFarm.Data
                 new Sensorvalue { Timestamp = new DateTime(2018, 10, 12, 15, 51, 0), Value = 250, SensorId = 1, PlantId = 1 },
                 new Sensorvalue { Timestamp = new DateTime(2018, 10, 13, 15, 51, 0), Value = 240, SensorId = 1, PlantId = 1 },
                 new Sensorvalue { Timestamp = new DateTime(2018, 10, 15, 15, 51, 0), Value = 250, SensorId = 1, PlantId = 1 },
-                new Sensorvalue { Timestamp = new DateTime(2018, 10, 16, 15, 51, 0), Value = 260, SensorId = 1, PlantId = 1 }
+                new Sensorvalue { Timestamp = new DateTime(2018, 10, 16, 15, 51, 0), Value = 260, SensorId = 1, PlantId = 1 },
+                new Sensorvalue { Timestamp = new DateTime(2018, 10, 11, 15, 51, 0), Value = 250, SensorId = 2, PlantId = 1 },
+                new Sensorvalue { Timestamp = new DateTime(2018, 10, 12, 15, 51, 0), Value = 250, SensorId = 2, PlantId = 1 },
+                new Sensorvalue { Timestamp = new DateTime(2018, 10, 13, 15, 51, 0), Value = 240, SensorId = 2, PlantId = 1 },
+                new Sensorvalue { Timestamp = new DateTime(2018, 10, 15, 15, 51, 0), Value = 250, SensorId = 2, PlantId = 1 },
+                new Sensorvalue { Timestamp = new DateTime(2018, 10, 16, 15, 51, 0), Value = 260, SensorId = 2, PlantId = 1 }
             );
 
             context.SaveChanges();
