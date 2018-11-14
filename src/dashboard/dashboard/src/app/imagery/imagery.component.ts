8ba2fe22ce665ed1f4d0f514ba7b24bb61a4b99f
Guzse <MyNameIsGuzse@gmail.com>
2018-11-14 10:47:39 +0100

    Calculating Green-percentage and health-percentage

--- src/dashboard/dashboard/src/app/imagery/imagery.component.ts
+++ src/dashboard/dashboard/src/app/imagery/imagery.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ClarifaiService } from '../clarifai_service/clarifai.service';
+import * as Convert from 'color-convert'
+import * as hex from 'hex2dec';
+import * as clarifai from '../clarifai_service/clarifai.service';
 
 @Component({
     selector: 'app-imagery',
@@ -9,13 +11,90 @@ import { ClarifaiService } from '../clarifai_service/clarifai.service';
 
 export class ImageryComponent implements OnInit {
 
-    constructor(private _svc: ClarifaiService) { }
+    constructor(private _svc: clarifai.ClarifaiService) { }
 
     url:string;
-    ngOnInit() {
-        this.url = "https://us-east-1.tchyn.io/snopes-production/uploads/2017/06/Dieffenbachia_amoena_poison_fb.jpg";
-        this._svc.getColorValues(this.url).then( x =>
-            console.log(this._svc.data)
-        );}
+    Output:clarifai.RootObject;
+    ColorData:clarifai.Color[];
+    GreenPercentage:number;
+    PlantHealth:number;
+    Growth:number;
+    MinGreenPercentage:string;
+    MinPlantHealth:string;
+    MinGrowth:string;
+    ColorValue:HSL = {
+        hue:0,
+        saturation:0,
+        light:0
+    };
 
+    ngOnInit() {
+        this.pullData();
+    }
+
+    calculateGreenPercentage(hsl:HSL):number{
+        let value:number;
+        if (hsl.hue <  140 && hsl.hue > 70 )
+            value = ((1/hsl.light)*(hsl.saturation))*50;
+        else
+            value = 0;
+
+        if (value < 0) value = 0;
+        if (value > 100) value = 100;
+        value = parseFloat(value.toFixed(2));
+
+        console.log("GreenPercentage: " + value);
+        return value;
+    }
+
+    calculatePlantHealth(hsl:HSL):number{
+        let value:number = (((1/(hsl.light-50)) + hsl.saturation)/50)*100;
+
+        if (value < 0) value = 0;
+        if (value > 100) value = 100;
+        value = parseFloat(value.toFixed(2));
+
+        console.log("PlantHealth: " + value);
+        return value;
+    }
+
+    calculateHSL(rgbHex:string){
+        let r = hex.hexToDec("0x" + rgbHex.slice(1,3));
+        let g = hex.hexToDec("0x" + rgbHex.slice(3,5));
+        let b = hex.hexToDec("0x" + rgbHex.slice(5,7));
+        
+        //console.log("Red: " + r + "\r\nGreen: " + g + "\r\nBlue: " + b);
+
+        this.ColorValue.hue = Convert.rgb.hsl(r,g,b)[0];
+        this.ColorValue.saturation = Convert.rgb.hsl(r,g,b)[1];
+        this.ColorValue.light = Convert.rgb.hsl(r,g,b)[2];
+        
+        /*
+        console.log("Hue: " + this.ColorValue.hue + 
+                "\r\nSaturation: " + this.ColorValue.saturation + 
+                "\r\nLight: " + this.ColorValue.light);
+        */
+    }
+
+    pullData(){
+        this.url = "https://us-east-1.tchyn.io/snopes-production/uploads/2017/06/Dieffenbachia_amoena_poison_fb.jpg";
+        this._svc.getColorValues(this.url).then( x =>{
+            this.Output = x;
+            this.ColorData = x.outputs[0].data.colors;
+            this.calculateHSL(this.ColorData[length].w3c.hex);
+            this.GreenPercentage = this.calculateGreenPercentage(this.ColorValue);
+            this.PlantHealth = this.calculatePlantHealth(this.ColorValue);
+            this.Growth = 45.37;
+            this.MinGrowth = this.Growth + "%";
+            this.MinGreenPercentage = this.GreenPercentage.toFixed(0) + "%";
+            this.MinPlantHealth = this.PlantHealth.toFixed(0) + "%";
+            //console.log("PlantHealth%: " + this.MinPlantHealth + "\r\nGreenPercentage%: " + this.MinGreenPercentage);
+        });
+    }
+}
+
+interface HSL {
+    hue:number;
+    saturation:number;
+    light:number;
 }
--- src/dashboard/dashboard/src/polyfills.ts
+++ src/dashboard/dashboard/src/polyfills.ts
@@ -47,7 +47,9 @@ import 'core-js/es7/reflect';
 
 // Add global to window, assigning the value of window itself. 
 (window as any).global = window;
-
+(window as any).process = {
+    env: { DEBUG: undefined },
+  };
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
