86bbec51b0130c18f4d35e157839f90da7fec7ec
Seppe De Beule <seppe.debeule@telenet.be>
2018-12-07 11:31:15 +0100

    Fix voor kapotte dashboard

--- src/dashboard/dashboard/src/app/test/test.component.ts
+++ src/dashboard/dashboard/src/app/test/test.component.ts
@@ -12,10 +12,6 @@ export class TestComponent implements OnInit {
   constructor(private _svc:ClarifaiService) { }
 
   ngOnInit() {
-<<<<<<< HEAD
-    //this._svc.getColorValues("https://us-east-1.tchyn.io/snopes-production/uploads/2017/06/Dieffenbachia_amoena_poison_fb.jpg");
-=======
     
->>>>>>> Dashboard_Clarifai
   }
 }
